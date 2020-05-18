b25371a6ff672ee4e877c0fd24efd8c4b3579a4b
gl <1003086124@qq.com>
2020-05-18 14:06:52 +0800

    测试idea集成git提交到远程库

---
+++ src/main/java/com/gl/Test.java
@@ -0,0 +1,13 @@
+package com.gl;
+
+/**
+ * @author gl
+ * @time 2020-05-18 14:04
+ * @function :
+ * @step :
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello!!");
+    }
+}
